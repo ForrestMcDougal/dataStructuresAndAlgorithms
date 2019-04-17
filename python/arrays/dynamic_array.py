dc5f5472790d28a4fdf99a7b890a4a2c6e8b2b5d
Forrest McDougal <forrestmcdougal@gmail.com>
2019-04-17 14:30:47 -0400

    dynamic array implementation

---
+++ python/arrays/dynamic_array.py
@@ -0,0 +1,59 @@
+"""
+implementation of dynamic array
+"""
+
+import ctypes
+
+
+class DynamicArray(object):
+    def __init__(self):
+        self.n = 0
+        self.capacity = 1
+        self.A = self.make_array(self.capacity)
+
+    def __len__(self):
+        return self.n
+
+    def __getitem__(self, k):
+        if not 0 <= k < self.n:
+            raise IndexError('K is out of bounds')
+
+        return self.A[k]
+
+    def append(self, ele):
+        if self.n == self.capacity:
+            # double capacity
+            self._resize(2 * self.capacity)
+
+        self.A[self.n] = ele
+        self.n += 1
+
+    def _resize(self, new_cap):
+        # make new array
+        B = self.make_array(new_cap)
+
+        for k in range(self.n):
+            B[k] = self.A[k]
+
+        self.A = B
+        self.capacity = new_cap
+
+    def make_array(self, cap):
+        return (cap * ctypes.py_object)()
+
+
+if __name__ == "__main__":
+    arr = DynamicArray()
+    arr.append(1)
+    print(len(arr))
+    arr.append(2)
+    print(len(arr))
+    print(arr[1])
+    try:
+        print(arr[4])
+    except IndexError as x:
+        print(x)
+    print(arr.capacity)
+    for i in range(100):
+        arr.append(i)
+    print(arr.capacity)
